9a37c1eb7047738d0d1351534ff31557a5a70382
larianegiachini <lariane.bachega@gmail.com>
2026-02-27 08:19:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-// animação ao rolar
-function reveal() {
-  const reveals = document.querySelectorAll(".reveal");
-
-  reveals.forEach(el => {
-    const windowHeight = window.innerHeight;
-    const elementTop = el.getBoundingClientRect().top;
-
-    if (elementTop < windowHeight - 100) {
-      el.classList.add("active");
-    }
-  });
-}
-
-window.addEventListener("scroll", reveal);
-reveal();
-
-// cursor personalizado
-const cursor = document.querySelector(".cursor");
-
-document.addEventListener("mousemove", e => {
-  cursor.style.left = e.pageX + "px";
-  cursor.style.top = e.pageY + "px";
+document.querySelector("form").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Mensagem enviada com sucesso!");
 });
